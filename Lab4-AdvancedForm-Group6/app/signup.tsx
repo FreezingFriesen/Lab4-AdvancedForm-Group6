6b1e51ee08ab01b23921696e610db8953856ac92
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-03-14 20:35:49 -0600

    Done some work and copied signin stylesheet

--- Lab4-AdvancedForm-Group6/app/signup.tsx
+++ Lab4-AdvancedForm-Group6/app/signup.tsx
@@ -1,5 +1,5 @@
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, TextInput} from 'react-native';
 
 import { employeeSchema } from '@/schemas/employeeSchema';
 import * as yup from "yup";
@@ -9,16 +9,34 @@ import { Formik } from 'formik';
 export default function SignUpScreen() {
 
 
-    return 
-    (
+    return (
     <View>
         <Formik 
-        initialValues={{ name: "", email: "", password: "", confirmpassword: ""}}>
-
+initialValues={{ name: "", email: "", password: "", confirmpassword: ""}}>
+        onSubmit={(values) => {
+            console.log("You submitted: ", values);
+        }}
+        {({     
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+        }) => (
+            <View>
+                <Text>Full name: </Text>
+                <TextInput
+                    value={values.name}
+                    onChangeText={handleChange("name")}
+                />
+            </View>
+        )}
         </Formik>
 
     </View>
-    )
+    );
 
 }
 
