f3f907b911dfb1a6c84757fc5b87f5cb21e9724b
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-03-14 20:27:15 -0600

    Created SignUpSchema

--- Lab4-AdvancedForm-Group6/app/signup.tsx
+++ Lab4-AdvancedForm-Group6/app/signup.tsx
@@ -1,6 +1,25 @@
 
+import { StyleSheet, View } from 'react-native';
+
+import { employeeSchema } from '@/schemas/employeeSchema';
+import * as yup from "yup";
+import { Formik } from 'formik';
 
 
 export default function SignUpScreen() {
 
+
+    return 
+    (
+    <View>
+        <Formik 
+        initialValues={{ name: "", email: "", password: "", confirmpassword: ""}}>
+
+        </Formik>
+
+    </View>
+    )
+
 }
+
+const styles = StyleSheet.create({})
