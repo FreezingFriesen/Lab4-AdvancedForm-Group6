7cbdd978f74a78b9e964bc43aa3c07ad76f3779f
logan-gameing <Logan.Jones1@edu.sait.ca>
2026-03-24 10:41:15 -0600

    Reapply "Added access to sign in page"
    This reverts commit d76a3dc21d631f69d023797efa4b8aed01698fe9.

--- app/index.tsx
+++ app/index.tsx
@@ -17,6 +17,13 @@ export default function HomeScreen() {
           <Text style={styles.buttonText}>Go to Employee Form</Text>
         </TouchableOpacity>
       </Link>
+
+      <Link href="/signup" asChild>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Go to Sign Up Form</Text>
+        </TouchableOpacity>
+      </Link>
+
     </View>
   );
 }
